e8d550cd1ba977ab0180d1af905fc192ed9597cc
BetulBNY <betulbny@gmail.com>
2026-04-09 13:59:32 +0300

    schema creation file

---
+++ sql/create_schema.sql
@@ -0,0 +1,7 @@
+-- ====================================================================
+-- Create Schemas
+-- ====================================================================
+-- Create Silver Schema:
+CREATE SCHEMA IF NOT EXISTS silver;
+-- Create Gold Schema:
+CREATE SCHEMA IF NOT EXISTS gold;

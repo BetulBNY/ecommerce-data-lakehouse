701c60dc80e490177cf76124e24776460045b42c
BetulBNY <betulbny@gmail.com>
2026-04-09 14:50:43 +0300

    create dim_customers table

---
+++ sql/gold/ddl/dim_customers.sql
@@ -0,0 +1,17 @@
+-- ====================================================================
+-- Create 'gold.dim_customers' Table
+-- ====================================================================
+-- DROP TABLE IF EXISTS gold.dim_customers;
+CREATE TABLE IF NOT EXISTS gold.dim_customers (
+    customer_pk INT PRIMARY KEY, -- PK comes from Silver Layer
+    customer_id TEXT,
+    customer_unique_id TEXT,
+    city TEXT,
+    state TEXT,
+    zip_code INT,
+    latitude FLOAT,
+    longitude FLOAT,
+	-- Audit Columns: I added these two date columns to track when data flowed from Silver to Gold.
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);

b14f9b8f1f18e2d6e7367ffe7e3766e4917a91e3
BetulBNY <betulbny@gmail.com>
2026-04-15 12:15:16 +0300

    Order payments test has been updated to better align with Airflow.

--- sql/silver/tests/test_order_payments.sql
+++ sql/silver/tests/test_order_payments.sql
@@ -1,33 +1,42 @@
--- ====================================================================
--- Quality Checks for silver.order_payments
--- ====================================================================
+/*
+===============================================================================
+PROFESSIONAL DATA QUALITY CHECK: silver.order_payments
+===============================================================================
+Logic:
+    Returns '1' if all checks pass, '0' if any validation fails.
+===============================================================================
+*/
 
--- 1. TEST: Duplicate Payment Records
--- EXPECTATION: Each order should have the same payment sequence only once.
+WITH test_results AS (
+    SELECT
+        -- TEST 1: Duplicate Payment Records (Expectation: 0. Each order should have the same payment sequence only once)
+        (SELECT COUNT(*) FROM (
+            SELECT order_id, payment_sequential 
+			FROM silver.order_payments 
+			GROUP BY 1, 2 
+			HAVING COUNT(*) > 1
+        ) t) AS count_duplicate_errors,
+
+        -- TEST 2: Invalid Payment Values (Expectation: 0) (Note: We accepted 0 values due to voucher/cancellation, but negative is impossible)
+        (SELECT COUNT(*) FROM silver.order_payments 
+         WHERE payment_value < 0
+        ) AS count_negative_value_errors,
+
+        -- TEST 3: Invalid Installments (Expectation: 0) Number of installments cannot be negative.
+        (SELECT COUNT(*) FROM silver.order_payments 
+         WHERE payment_installments < 0
+        ) AS count_negative_installment_errors,
+
+        -- TEST 4: Payment Type Standardization (Expectation: 0) 'not_defined' should not remain and names should be in INITCAP (Credit Card etc.)
+        (SELECT COUNT(*) FROM silver.order_payments 
+         WHERE payment_type = 'Not_defined' 
+            OR payment_type != INITCAP(payment_type)
+        ) AS count_standardization_errors
+)
 SELECT 
-    order_id, 
-    payment_sequential, 
-    COUNT(*) 
-FROM silver.order_payments 
-GROUP BY order_id, payment_sequential 
-HAVING COUNT(*) > 1;
-
--- 2. TEST: Invalid Payment Values
--- EXPECTATION: Payment amount should not be negative.
--- (Note: We accepted 0 values due to voucher/cancellation, but negative is impossible).
-SELECT * 
-FROM silver.order_payments 
-WHERE payment_value < 0;
-
--- 3. TEST: Invalid Installments
--- EXPECTATION: Number of installments cannot be negative.
-SELECT * 
-FROM silver.order_payments 
-WHERE payment_installments < 0;
-
--- 4. TEST: Payment Type Standardization
--- EXPECTATION: 'not_defined' should not remain and names should be in INITCAP (Credit Card etc.).
-SELECT DISTINCT payment_type 
-FROM silver.order_payments 
-WHERE payment_type = 'not_defined' 
-   OR payment_type != INITCAP(payment_type);
+    CASE 
+        WHEN (count_duplicate_errors + count_negative_value_errors + 
+              count_negative_installment_errors + count_standardization_errors) = 0 THEN 1
+        ELSE 0 
+    END AS validation_status
+FROM test_results;

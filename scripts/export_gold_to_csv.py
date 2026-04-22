5c36b5509d8e4c325df24524d847a0b18e7aea0a
BetulBNY <betulbny@gmail.com>
2026-04-22 14:22:15 +0300

    dashboard scripts updated.

--- scripts/export_gold_to_csv.py
+++ scripts/export_gold_to_csv.py
@@ -24,13 +24,21 @@ def export_to_csv():
         df = pd.read_sql(f"SELECT * FROM {view}", engine)
         df.to_csv(f"{output_dir}/{filename}", index=False)
 
-    print(f"Files in {output_dir}: {os.listdir(output_dir)}")
     # 2. Save Custom Metrics (Gauge and Avg Delivery)
     # These are single-row values, so I can gather them all in a 'summary.csv' file. This file will contain the average review score and the average delivery days, which are key metrics for my dashboard(BNY).
     summary_data = {
         "avg_review_score": pd.read_sql("SELECT AVG(review_score) as val FROM gold.fact_orders", engine).iloc[0,0],
         "avg_delivery_days": pd.read_sql("SELECT AVG(delivery_time_days) as val FROM gold.fact_orders WHERE order_status = 'Delivered'", engine).iloc[0,0],
-        "retention_rate": pd.read_sql("...(yukarıdaki sorgu)...", engine).iloc[0,0]
+        "retention_rate": pd.read_sql("""
+        WITH customer_orders AS (
+            SELECT customer_unique_id, COUNT(order_id) as order_count
+            FROM gold.dim_customers c
+            JOIN gold.fact_orders f ON c.customer_pk = f.customer_pk
+            GROUP BY 1
+        )
+        SELECT (COUNT(*) FILTER (WHERE order_count > 1)::numeric / COUNT(*)) * 100 as retention_rate
+        FROM customer_orders
+    """, engine).iloc[0,0]
     }
     pd.DataFrame([summary_data]).to_csv(f"{output_dir}/summary_metrics.csv", index=False)
     # 3. Save Map Data (Customer Locations)
@@ -44,4 +52,5 @@ def export_to_csv():
     df_map = pd.read_sql(map_query, engine)
     df_map.to_csv(f"{output_dir}/dim_customers_sample.csv", index=False)
     
+    print(f"Files in {output_dir}: {os.listdir(output_dir)}")
     print("All necessary dashboard data (Views, Metrics, Map) exported successfully!")
